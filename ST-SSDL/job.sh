fe46c764a3d85f5317d1cc953c0d267347559ad4
kienle141204 <letrungkien14122004@gmail.com>
2026-01-16 17:13:03 +0700

    fix

--- ST-SSDL/job.sh
+++ ST-SSDL/job.sh
@@ -1,25 +1,4 @@
-#!/bin/bash -l
-
-#SBATCH --job-name=test-stllm
-#SBATCH --comment="user03"
-
-#SBATCH --partition=defq
-#SBATCH --output=logs/%x_%j.out
-#SBATCH --error=logs/%x_%j.err
-#SBATCH --nodelist=dgx02
-##SBATCH --time=14-00:00:00
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=16
-#SBATCH --mem-per-cpu=10g
-#SBATCH --gres=gpu:1
-
-source ~/miniconda3/etc/profile.d/conda.sh
-conda activate stllm-test1
-export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
-export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:512
-
-cd home/user03/VARDiff-test/STLLM-baseline/ST-SSDL
+conda activate bl-stssdl
 
 python generate_training_data_his_PEMS.py --dataset PEMS04
 python generate_training_data_his_PEMS.py --dataset PEMS07
